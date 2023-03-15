f7eafd637a335dbc5183ec8b92032bf09b82c0aa
tsg0o0 <tsg0o0@tsg0o0.com>
2023-03-15 22:51:47 +0900

    zopfli test

--- Main/main.go
+++ Main/main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"os"
+	"os/exec"
 )
 
 func main() {
+	arg := os.Args[1]
+	pngCompress(arg)
+}
+
+func pngCompress(inputFile string) {
+	originalInfo, _ := os.Stat(inputFile)
+	fmt.Println("Compressing... (by zopfli)")
+	
+	cmd := exec.Command("zopflipng", "-m", "-y", inputFile, inputFile)
+	
+	// run zopflipng
+	if runtime.GOOS == "darwin" {
+		cmd = exec.Command("./resources/mac/zopflipng", "-m", "-y", inputFile, inputFile)
+	}else if runtime.GOOS == "linux" {
+		cmd = exec.Command("./resources/linux/zopflipng", "-m", "-y", inputFile, inputFile)
+	}else if runtime.GOOS == "windows" {
+		cmd = exec.Command("./resources/win/zopflipng", "-m", "-y", inputFile, inputFile)
+	}
+	err := cmd.Run()
+	if err != nil {
+		//Failed
+		fmt.Println("Failed. (by zopfli): ", err)
+	}else{
+		//Success
+		resultInfo, _ := os.Stat(inputFile)
+		fmt.Println("Success. (by zopfli)")
+		fmt.Println("Original file size:", originalInfo.Size())
+		fmt.Println("Result file size:", resultInfo.Size())
+		fmt.Println("Percentage of original", ( ( 100 * resultInfo.Size() ) / originalInfo.Size() ), "%")
+	}
+	
 }
 
